cc9e4993c8a9a3ba6033099a3d561cd19c22eb0f
sailaja <sailaja.sightspectrum@gmail.com>
2023-06-05 22:56:42 +0530

    first commit

---
+++ week1-task1/practise-1.py
@@ -0,0 +1,49 @@
+
+#LIST
+##thislist = ["apple", "banana", "cherry"]
+##print(thislist)
+##
+#STRING
+
+##print("Hello")
+##print('Hello')
+##a = "Hello"
+##print(a)
+
+###COUNT
+##txt = "I love apples, apple are my favorite fruit"
+##
+##x = txt.count("apple")
+##
+##print(x)
+##
+##txt = "I love apples, apple are my favorite fruit"
+##
+##x = txt.count("apple", 10, 24)
+
+
+###TUPLE
+##print(x)
+##thistuple = ("apple", "banana", "cherry")
+##print(thistuple)
+
+
+###BOOLEAN
+##print(10 > 9)
+##print(10 == 9)
+##print(10 < 9)
+##
+
+#CAPATILIZE
+##txt = "hello, and welcome to my world."
+##
+##x = txt.capitalize()
+##
+##print (x)
+
+#VAR
+##x = 5
+##y = "John"
+##print(x)
+##print(y)
+##
