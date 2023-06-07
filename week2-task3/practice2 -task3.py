e39f86e77455204f4c3e8d293e4360c5576a9c04
sailaja <sailaja.sightspectrum@gmail.com>
2023-06-07 17:45:18 +0530

    first commit

---
+++ week2-task3/practice2 -task3.py
@@ -0,0 +1,72 @@
+##....................dict methods............................
+
+
+
+##..............................copy().............................
+##   
+
+##var={"love":"fake"}
+##x=var.copy()
+##print(x)
+##
+##
+##var={1256:"uwywucnsowehsdhf"}
+##y=var.copy()
+##print(x)
+
+
+
+#from key
+##x = ('key1', 'key2', 'key3')
+##y = 0
+##
+##thisdict = dict.fromkeys(x, y)
+##
+##print(thisdict)
+
+
+##x=('sai1','sai2','sai3')
+##y=5
+##thisdict=dict.fromkeys(x,y)
+##print(thisdict)
+
+   ##GET()
+##car = {
+##  "brand": "Ford",
+##  "model": "Mustang",
+##  "year": 1964
+##}
+##
+##x = car.get("model")
+##
+##print(x)
+
+
+
+##var={"name":"name","age":23,1.000:"jaga",1:"one"}
+##print(var)
+
+##key
+##
+##x={"name":22,"hello":22}
+##y=x.keys()
+##print(y)
+
+
+#VALUES
+
+
+x={"name":33,"apple":66}
+y=x.values()
+print(y)
+
+
+
+
+
+
+
+
+####name={"fruit":"apple","laddu":"sweet"}
+####print(name)
+##
