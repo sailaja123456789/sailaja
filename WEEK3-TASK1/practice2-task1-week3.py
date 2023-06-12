87d97db582c1a95e44e94efce0a90926dbe0f2dd
sailaja <sailaja.sightspectrum@gmail.com>
2023-06-12 17:58:42 +0530

    first commit

---
+++ WEEK3-TASK1/practice2-task1-week3.py
@@ -0,0 +1,357 @@
+#if else
+
+##Equals: a == b
+##Not Equals: a != b
+##Less than: a < b
+##Less than or equal to: a <= b
+##Greater than: a > b
+##Greater than or equal to: a >= b
+
+##a = 33
+##b = 200
+##
+##if b > a:
+##   print("b is greater than a")
+##
+##
+##a = 22
+##b = 2000
+##if b > a:
+##   print("b is greater than a")
+##
+##
+##
+##
+##x = "sailaja"
+##y = "gayathri"
+##
+##if x > y:
+##   print("x is greater than y")
+##
+##x = "sailaja"
+##y = "gayathri"
+##
+##if x > y:
+   #print("y is greater than x")
+
+##a = 34
+##b = 34
+##c = 23
+##d = 545
+##
+##if a==b!=c<=d:
+##   print("a is equal to b not equal to c is greater than d")
+##
+##
+##
+##x=43
+##y=43
+##if x==y:
+##   print("x is equal to y" )
+##
+##
+##x=44
+##y=45
+##if x!=y:
+##    print("x is not equal to y")  
+##
+##
+##a="sailaja"
+##b="jgiurtrkfitkfgweeriuwhh"
+##if a!=b:
+##    print("a is not equal to b")
+##
+##a="fhkfrudyfuey"
+##b="ejoweiweiwwoy"
+##if a>b:
+## print("a is greter than b")
+##
+##
+##a=5645
+##b=546454
+##if a>b:
+##    print ("a is greter than b")
+##
+##
+##
+##a="sailaja"
+##b="gayathri"
+##if a>=b:
+##    print ("a is greater than is equal to b")
+##
+##
+##a=454
+##b=544
+##if a<=b:
+##  print("a is less than is equal to b")
+
+
+##x=234
+##y=234
+##if a<=b:
+##    print("a is less than is equal to b")
+##
+##a="jhanu"
+##b="sailaja"
+##if a<b:
+##    print("a is less than  b")
+
+
+##a=233545454454
+##b=433
+##if a>b:
+##    print("a is greter than b")
+##
+##
+##
+##a=656565665565
+##b=65654545454
+##if a<b:
+##    print("a is less than b")
+
+
+##a=34222
+##b=433543364
+##if a<b:
+##    print("a is less than b")
+
+
+
+
+
+
+
+
+##x={"sailaja":"name","age":22}
+##y=x.items()
+##print(y)
+
+
+
+
+
+#for loop
+
+##a="banana","mango","grapes"
+##for x in a:
+##  print(x)
+##
+##for x in (2,30):
+##    print(x)
+##
+##
+##for x in range (2,20,4):
+##   print(x)
+##
+##
+##v="gayathri","jhansi","sailaja"
+##for x in v:
+##     print(x)
+
+
+##for x in "banana":
+##  print(x)
+##
+##  
+##for y in "mango":
+##   print(y)
+##
+##for a in "apple":
+##    print(a)
+
+
+
+##fruits = ["apple", "banana", "cherry"]
+##for x in fruits:
+##  print(x)
+##  if x == "banana":
+##    break
+##
+##
+##name=["gaythri","sailaja","jhansi","divya"]
+##
+##for x in name:
+##    print(x)
+##
+##    if x == "jhansi":
+##        break
+##
+##
+##var=[2353,54645,54654347,865478,74]
+##for x in var:
+##    print(x)
+
+##    if x == 865478:
+##
+##        break
+##
+##
+##
+##fruits = ["water millon", "lilly", "mango"]
+##for x in fruits:
+##  if x == "lilly":
+##    continue
+##  print(x) 
+##
+##
+##
+##
+##
+##fruits = ["apple", "banana", "cherry"]
+##for x in fruits:
+##  if x == "banana":
+##    continue
+##  print(x) 
+##
+##
+##
+##names=["sailaja","arya","devil","mahi"]
+##for x in names:
+##    print(x)
+##    
+##    if x == "devil":
+##        
+##        continue
+##      
+
+##for x in range(6):
+##  print(x) 
+##
+##for x in range (8):
+##    print(x)
+##
+##
+##for a in range(4):
+##    print(a)
+##
+##for a in range(7):
+##    print(a)
+##
+
+##
+##for x in range(2, 6):
+##  print(x) 
+##
+##
+##for a in range(4,20):
+##    print(a)
+##
+##
+##
+##for a in range(2,10,3):
+##    print(a)
+##
+##for x in range(1,20,2):
+##    print(x)
+
+##
+##for x in range(6):
+##  print(x)
+##else:
+##  print("Finally finished!")
+##
+##
+##for x in range(4):
+##    print(x)
+##else:
+##   print("finally finished")
+
+
+
+##for x in range(6):
+##  if x == 3: break
+##  print(x)
+##else:
+##  print("Finally finished!")
+##
+##
+##for x in range(8):
+##    if x==4: break
+##    print(x)
+##else:
+##    print("it's completed")
+
+
+
+##adj = ["red", "big", "tasty"]
+##fruits = ["apple", "banana", "cherry"]
+##
+##for x in adj:
+##  for y in fruits:
+##    print(x, y)
+##
+##name=["red","white","pink","orange"]
+##flowers=["rose","rose","rose","rose"]
+##
+##for x in name:
+##    for y in flowers:
+##        print(x,y)
+
+
+
+##for x in [0, 1, 2]:
+##  pass
+##
+##
+##
+##
+##for x in [2,3,4]:
+##    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+    
+
+
+
+
+
+
