0e37b9a12a185eb5ed6e9d0383aecdf3369bc510
sailaja <sailaja.sightspectrum@gmail.com>
2023-06-13 16:22:52 +0530

    first commit

---
+++ week3-task2/practise2-week3.py
@@ -0,0 +1,207 @@
+.................while loop......................
+x=1
+while x<7:
+    print (x)
+    x+=1
+
+x=2
+while x<8:
+    print (x)
+    x+=2
+
+x=3
+while x<9:
+    print (x)
+    x+=3
+
+x=1
+while x<10:
+    print(x)
+    x+=1
+
+n=2
+i=0
+while i<10:
+  if n%2==0 or n%7==0:
+    print(n)
+    i+=1
+    n+=1  
+
+n=3
+i=7
+while i<10:
+    if n%2==0 or i%7==0:
+        print(i)
+        i+=1
+        n+=1
+
+
+
+
+
+.........................break...........................
+
+x=1
+while x <8:
+    print(x)
+    if x==3:
+     break
+    x+=1
+
+x=2
+while x<4:
+    print(x)
+    if x==2:
+        break
+    x+=2
+
+x=0
+while x<10:
+    print(x)
+    if x==2:
+        break
+    x+=3
+    
+
+
+
+i = 1
+while i < 6:
+  print(i)
+  if (i == 3):
+    break
+  i += 1
+     
+i = 1
+while i < 6:
+  print(i)
+  if (i == 4):
+    break
+  i += 1
+
+
+..........................continue......................
+
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+
+x=0
+while x<9:
+    x+=1
+    if x==2:
+      continue
+    print(x)
+
+
+a=1
+while a<8:
+    a+=1
+    if a==3:
+      continue
+    print(a)
+
+
+var=1
+while var<10:
+    var+=1
+    if var==1:
+        continue
+    print(var)
+
+x=2
+while x<9:
+    x+=1
+    if x==2:      
+        continue
+    print(x)
+
+
+................else.................
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+
+a=2
+while a<5:
+    print(a)
+    a+=1
+else:
+        print("a is no longer less than 5")
+        
+
+
+a=2
+while a<8:
+    print(a)
+    a+=2
+else:
+    print("a is no longer less than 8")
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
