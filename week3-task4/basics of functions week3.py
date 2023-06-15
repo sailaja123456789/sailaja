2d8dd86f2ccdd49d392c9acda31bcbf34601ae1d
sailaja <sailaja.sightspectrum@gmail.com>
2023-06-15 16:57:15 +0530

    first commit

---
+++ week3-task4/basics of functions week3.py
@@ -0,0 +1,265 @@
+def samplefunction():
+    print ("function created successfully")
+
+
+samplefunction()
+samplefunction()
+samplefunction()
+samplefunction()
+samplefunction()    
+
+def addition():
+    a=10
+    b=10
+    c=a+b
+    print(c)
+addition()
+add1=addition()
+print(add1)
+
+def divison    
+
+
+
+def addnumbers(number1,number2):
+    return number1+number2
+print(addnumbers(4,5))
+
+
+def mulnumbers(number1,number2):
+    return number1*number2
+print(subnumbers(4,5))
+
+def divnumbers(number1,number2):
+    return number1%number2
+print(divnumbers(4,2))
+
+
+def subnumbers(number1,number2):
+    return number1-number2
+print(subnumbers(20,15))
+
+
+def numbers(number1,number2):
+    return number1-number2*number1%number2
+print(numbers(45,57,))
+
+
+def greet():
+    print("hello")
+    print("good afternoon")
+
+greet()
+
+def add(x,y):
+    c=x+y
+    print(c)
+greet()
+add(4,5)
+          
+
+def numbers(number1,number2,number3,number4):
+    return number1-number2+number3*number4%number1+number2
+print(numbers(23,45,65,75))
+
+
+
+
+
+............................FOR LOOP......................................
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x) 
+
+print(type(fruits))
+
+names=["divya","gayathri","jhansi"]
+for x in names:
+  print(x)
+
+number=[23,45,67,"rtueoitfdhfueoeruturtfepo"]
+for x in number:
+  print(x)
+
+number={"sailja","anna"}
+for x in number:
+  print(type(x))
+
+var=("rose","rose","flower","fruits")
+for x in var:
+  print(x)
+
+..........BREAK STATEMENT.....
+  
+var=["sailaja","jhansi","gayathri","divya","hguruirtet84pow293fdjghrfjfghrtr"]
+for x in var:  
+  print(x)
+  if x== "gayathri":
+    break
+
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+numbers=23,43,45,65
+for x in numbers:
+  print(x)
+  if x==45:
+    break
+
+
+EXAMPLE2
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+
+number=34,564,6,7575657,"tyqwoiuwsncduhe"
+for x in number:
+  if x==6:
+   break
+  print(x)
+
+.............CONTINUE........
+
+flowers=["rose","lilly","red rose","hugrtrutrrj"]
+for x in flowers:
+  if x=="red rose":
+    continue
+  print(x)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  
+  if x == "banana":
+    continue
+  print(x)
+
+fruits = ["apple", "banana", "cherry","mango"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x) 
+
+
+....................RANGE()FUNCTION..........
+
+
+for x in range(6):
+  print(x) 
+
+
+for a in range(8):
+  print(a)
+
+for x in range(1,3):
+  print(x)
+
+for x in range(10):
+  print(x)
+
+
+
+................................................WHILE LOOP......................
+
+count = 0
+while count < 5:
+    print("Count:", count)
+    count += 1
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
+BREAK
+
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+CONTINUE
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+ELSE 
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+else:
+  print("i is no longer less than 6")
+
+
+
+
+....................................IF , ELIF,AND ELSE STATEMENT.....................
+
+a = 33
+b = 200
+
+if b > a:
+  print("b is greater than a")
+
+
+a=22
+b=300
+if b>a:
+  print("b is greater than a")
+
+a=300
+b=44
+if b<a:
+  print("b is less than a" )
+
+
+.........INDENTATION............
+
+a = 33
+b = 200
+if b > a:
+ print("b is greater than a")
+
+
+ELIF
+a = 33
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+
+
+ELSE
+
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+
+
+
+
+
+
+
+
+
