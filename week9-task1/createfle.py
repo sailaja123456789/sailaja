98d995878cdfc4658093b8d7d2befd598b23d44d
sailaja <sailaja.sightspectrum@gmail.com>
2023-07-18 12:40:29 +0530

    first commit

---
+++ week9-task1/createfle.py
@@ -0,0 +1,43 @@
+##f=open("sample.txt","w")
+##f=open("sample.txt","w")
+##f.write("my name is shailaja")
+##f.close()
+##
+f=open("arya.txt","w")
+f.write("my name is arya")
+f.close()
+
+f=open("arya.txt","r")
+a=f.read()
+f.close()
+print(a)
+
+f=open("hello world.txt","w")
+f.write("welcome to my world")
+f.close()
+
+f=open("hello world.txt","r")
+a=f.read()
+f.close()
+print(a)
+
+
+##f=open("hi arya.txt","w")
+##f.write("hi arya")
+##f.close()
+##
+##
+##f=open("hi arya.txt","r")
+##a=f.read()
+##f.close()
+##print(a)
+##
+##
+##f=open("sailu.txt","w")
+##f.write("good morning amma")
+##f.close()
+##
+##f=open("sailu.txt","r")
+##x=f.read()
+##f.close()
+##print(x)
