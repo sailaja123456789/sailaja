2b97c626c39affd1abfc64ed582af7da8cf28c30
sailaja <sailaja.sightspectrum@gmail.com>
2023-07-26 18:16:28 +0530

    first commit

---
+++ week10-task3/date and time practise.py
@@ -0,0 +1,172 @@
+import datetime
+cd=datetime.datetime.now()
+res=cd.strftime("%y")
+print("short version of year:" ,res)
+
+
+import datetime
+cd=datetime.datetime.now()
+res=cd.strftime("%Y")
+print("long version of year:",res)
+
+
+
+import datetime
+cd=datetime.datetime.now()
+res=cd.strftime("%Y")
+print("long version of year:",res)
+
+
+
+import datetime
+cd=datetime.datetime.now()
+
+res=cd.strftime("%w")
+print("short version of month:",res)
+
+
+import datetime
+cd=datetime.datetime.now()
+
+
+res=cd.strftime("%W")
+print("short version of month:",res)
+
+
+import datetime
+cd=datetime.datetime.now()
+
+res=cd.strftime("%a")
+print("short version of day:",res)
+
+
+import datetime
+cd=datetime.datetime.now()
+
+res=cd.strftime("%A")
+print("short version of day:",res)
+
+
+import datetime
+cd=datetime.datetime.now()
+res=cd.strftime("%H")
+print("short version of hours:",res)
+
+
+
+import datetime
+cd=datetime.datetime.now()
+
+res=cd.strftime("%I")
+print("short version of hours:",res)
+
+
+
+import datetime
+cd=datetime.datetime.now()
+
+res=cd.strftime("%p")
+print("short version of time:",res)
+
+
+
+import datetime
+cd=datetime.datetime.now()
+
+res=cd.strftime("%M")
+print("short version of minute:",res)
+
+import datetime
+cd=datetime.datetime.now()
+
+
+res=cd.strftime("%S")
+print("short version of second:",res)
+
+
+
+import datetime
+cd=datetime.datetime.now()
+
+res=cd.strftime("%f")
+print("short version of month:",res)
+
+
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+
+
+
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
+
+
+
+import datetime
+
+x = datetime.datetime(2023, 5, 17)
+
+print(x)
+
+
+
+import datetime
+
+x = datetime.datetime(2022, 5, 17)
+
+print(x)
+
+
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%B"))
+
+
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%S"))
+
+
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%A"))
+
+
+import datetime
+
+x = datetime.datetime(2018, 6, 1)
+
+print(x.strftime("%f"))
+
+
+import datetime
+
+x=datetime.datetime(2016,7,7)
+print(x.strftime("%A"))
+
+
+
